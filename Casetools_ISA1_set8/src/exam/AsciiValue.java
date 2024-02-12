694ce8d4fa181e85a504b2193570ce99892328ac
Errol Caeiro <errolcaeiro07@gmail.com>
2024-02-12 09:02:14 +0530

    Initial commit

---
+++ Casetools_ISA1_set8/src/exam/AsciiValue.java
@@ -0,0 +1,15 @@
+package exam;
+
+public class AsciiValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char ch = 'a';
+		int ascii = ch;
+		int castAscii = (int)ch;
+		System.out.println("The ASCII value of " +ch + " is:" +ascii);
+		System.out.println("The ASCII value of" +ch + " is:" +castAscii);
+	}
+
+}
